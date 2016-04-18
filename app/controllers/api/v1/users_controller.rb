755415a698ce4ba5ea57a1a00c6ca40ccd7da972
liuyuan <fenjin1987@gmail.com>
2016-04-18 16:39:10 +0800

    昨天在我机器上报错的修改

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,14 @@
 class Api::V1::UsersController < Api::V1::BaseController
 
   before_action :authenticate_user!, only: [:update]
+  # def index
+  #   @users = User.all
+  # end
 
   def show
     @user = User.find(params[:id])
   end
-    
+
   def update
     @user = User.find(params[:id])
     # return api_error(status: 403) if !UserPolicy.new(current_user, @user).update?
---
+++ app/views/api/v1/users/index.json.jbuilder
@@ -0,0 +1,3 @@
+# json.users do |user|
+#   json.(@users, :id, :email, :name,  :activated, :admin, :created_at, :updated_at)
+# end
